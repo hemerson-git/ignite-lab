25144c334f2493bd6e84962c3f6f95502b3b2c1d
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-06-22 22:57:48 -0300

    CREATE sidebar, ADD date-fns, ADD phosphor-react

--- ignite-lab-hemesron/src/App.tsx
+++ ignite-lab-hemesron/src/App.tsx
@@ -2,35 +2,13 @@ import { gql, useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { client } from "./lib/apollo";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        id
-        name
-      }
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+// Components
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
     <div className="App">
-      <ul>
-        {data?.lessons.map((lesson) => (
-          <li key={lesson.id}>{lesson.title}</li>
-        ))}
-      </ul>
+      <Event />
     </div>
   );
 }
