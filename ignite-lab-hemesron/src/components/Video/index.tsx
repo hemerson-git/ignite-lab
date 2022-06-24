fa328ded0b703fb66853c2617332372642f6e147
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-06-24 02:54:53 -0300

    ADD vimejs lib, Make videos dynamic

--- ignite-lab-hemesron/src/components/Video/index.tsx
+++ ignite-lab-hemesron/src/components/Video/index.tsx
@@ -1,4 +1,5 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import {
   CaretRight,
   DiscordLogo,
@@ -8,13 +9,65 @@ import {
 
 import "@vime/core/themes/default.css";
 
-export function Video() {
+const GET_LESSONS_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      description
+      title
+      videoId
+      teacher {
+        avatarURL
+        name
+        bio
+      }
+    }
+  }
+`;
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+
+    teacher: {
+      name: string;
+      bio: string;
+      avatarURL: string;
+    };
+  };
+}
+
+export function Video({ lessonSlug }: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(
+    GET_LESSONS_BY_SLUG_QUERY,
+    {
+      variables: {
+        slug: lessonSlug,
+      },
+
+      fetchPolicy: "no-cache",
+    }
+  );
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <h2>Loading</h2>
+      </div>
+    );
+  }
+
   return (
     <section className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-          <Player>
-            <Youtube videoId="_ITiwPMUzho" />
+          <Player isPiPActive>
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -23,31 +76,26 @@ export function Video() {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
 
             <p className="mt-4 text-gray-100 leading-relaxed">
-              Chegamos na metade do nosso evento, mas ainda tem mais pela
-              frente…Na terceira aula vamos continuar nosso projeto,
-              desenvolvendo o roteamento e player. Essa é mais uma etapa para
-              sua especialização em React!
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
-                src="https://github.com/hemerson-git.png"
-                alt=""
+                src={data.lesson.teacher.avatarURL}
+                alt={`Foto de ${data.lesson.teacher.name}`}
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
               />
 
               <div>
                 <strong className="font-bold text-2xl block">
-                  Hemerson Oliveira
+                  {data.lesson.teacher.name}
                 </strong>
 
                 <span className="text-gray-200 text-sm block">
-                  Frontend iZap
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
--- ignite-lab-hemesron/src/pages/Event/index.tsx
+++ ignite-lab-hemesron/src/pages/Event/index.tsx
@@ -1,14 +1,22 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Video } from "../../components/Video";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="page_event flex flex-col min-h-screen">
       <Header />
 
       <main className="flex flex-1">
-        <Video />
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex-1 bg-black"></div>
+        )}
+
         <Sidebar />
       </main>
     </div>
