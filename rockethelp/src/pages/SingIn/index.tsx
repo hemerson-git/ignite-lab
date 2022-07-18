0dcf376b6ed080dad4189ff32fded38f9f6e3695
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-07-18 01:03:25 -0300

    CREATE toggle visible button

--- rockethelp/src/pages/SingIn/index.tsx
+++ rockethelp/src/pages/SingIn/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { Heading, VStack, Icon, useTheme } from "native-base";
-import { Envelope, Key } from "phosphor-react-native";
+import { Envelope, Key, Eye, EyeClosed } from "phosphor-react-native";
 
 import Logo from "../../assets/logo_primary.svg";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { TouchableOpacity } from "react-native";
 
 export function SignIn() {
   const { colors } = useTheme();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  function togglePasswordVisibility() {
+    setPassword(password === "" ? "password" : "");
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -18,6 +27,8 @@ export function SignIn() {
 
       <Input
         placeholder="E-mail"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
         mb={4}
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
@@ -26,9 +37,27 @@ export function SignIn() {
 
       <Input
         placeholder="Senha"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
         mb={8}
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
-        secureTextEntry
+        InputRightElement={
+          <TouchableOpacity
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+          >
+            <Icon
+              mr={4}
+              as={
+                !isPasswordVisible ? (
+                  <Eye color={colors.gray[300]} />
+                ) : (
+                  <EyeClosed color={colors.gray[300]} />
+                )
+              }
+            />
+          </TouchableOpacity>
+        }
+        secureTextEntry={!isPasswordVisible}
       />
 
       <Button title="Entrar" w="full" />
