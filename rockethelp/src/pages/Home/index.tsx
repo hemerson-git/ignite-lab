6df34f557bb18d9d54f46ce85f5f7ab8a2808447
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-07-22 11:20:31 -0300

    ADD Logout funtion

--- rockethelp/src/pages/Home/index.tsx
+++ rockethelp/src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ import {
 import { ChatTeardropText, SignOut } from "phosphor-react-native";
 import { useTheme } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
 
 // Components
 
@@ -18,40 +19,14 @@ import Logo from "../../assets/logo_secondary.svg";
 import { Filter } from "../../components/Filter";
 import { Order, OrderProps } from "../../components/Order";
 import { Button } from "../../components/Button";
+import { Alert } from "../../components/Alert";
 
 export function Home() {
   const { colors } = useTheme();
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
   const [orders, setOrders] = useState<OrderProps[]>([
     {
       id: "123",
-      patrimony: "123456",
-      when: "18/05/2022",
-      status: "open",
-    },
-
-    {
-      id: "124",
-      patrimony: "123456",
-      when: "18/05/2022",
-      status: "closed",
-    },
-
-    {
-      id: "125",
-      patrimony: "123456",
-      when: "18/05/2022",
-      status: "open",
-    },
-
-    {
-      id: "126",
-      patrimony: "123456",
-      when: "18/05/2022",
-      status: "open",
-    },
-
-    {
-      id: "127",
       patrimony: "123456",
       when: "18/05/2022",
       status: "open",
@@ -70,8 +45,23 @@ export function Home() {
     navigation.navigate("details", { orderId });
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch((error) => {
+        setIsAlertModalOpen(true);
+        console.log(error);
+      });
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
+      <Alert
+        isOpen={isAlertModalOpen}
+        title="Ocorreu um erro ao tentar fazer logout"
+        dismiss={() => setIsAlertModalOpen(false)}
+      />
+
       <HStack
         w="full"
         justifyContent="space-between"
@@ -83,7 +73,10 @@ export function Home() {
       >
         <Logo />
 
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          onPress={handleLogout}
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
