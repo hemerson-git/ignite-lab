e9341bae6a02ebafd7d338cb1cdd3abb890a2e57
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-07-22 02:05:07 -0300

    ADD Routes Validations

--- rockethelp/src/routes/index.tsx
+++ rockethelp/src/routes/index.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+
+// Components
 
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../pages/SingIn";
+import { Loading } from "../components/Loading";
 
 export function Routes() {
-  return (
-    // <NavigationContainer>
-    //   <AppRoutes />
-    //   {/* <AppRoutes /> */}
-    // </NavigationContainer>
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
-    <SignIn />
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <NavigationContainer>
+      {user ? <AppRoutes /> : <SignIn />}
+    </NavigationContainer>
   );
 }
